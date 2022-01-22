c42d34b5f79285d71ea2ce5f72db81751b62d794
Lorcan Mc <lorcan.jmc@gmail.com>
2022-01-22 14:50:57 +0000

    order by id rather than timestamp

--- models/tweets.js
+++ models/tweets.js
@@ -1,7 +1,7 @@
 import query from "../db/index.js";
 
 export async function getAllTweets() {
-    const data = await query(`SELECT * FROM tweets ORDER BY timestamp DESC LIMIT 10;`);
+    const data = await query(`SELECT * FROM tweets ORDER BY id DESC LIMIT 10;`);
     return data.rows;
 }
 
